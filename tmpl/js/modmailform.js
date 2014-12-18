1e50872488067c0e202c9d75a0b622868d94a463
Petrov <Petrov@ttcsoft.ru>
2014-12-18 17:33:29 +0400

    Revert "0.8.1: - Исправлены ошибки не работающих кнопок "Отправка" и "Повтор""
    This reverts commit ad8ac8b5747007c520d89f36888176f8333da8d4.

--- tmpl/js/modmailform.js
+++ tmpl/js/modmailform.js
@@ -54,7 +54,7 @@ ModMailform.addEvents = function(settings) {
 	this.setFormSizes(moduleId);
 	// Отправка формы на сервер
 	jQuery("#" + ModMailform.FORM_ID + "_" + moduleId).on("submit", function() {
-		ModMailform.sendMessage(moduleId);
+		ModMailform.sendMessage(moduleId, baseUri, moduleName);
 	});
 	// Открытие окна отправки собщений
 	jQuery("#" + this.FORM_OPEN_BUTTON_ID + "_" + moduleId).on(
@@ -105,10 +105,14 @@ ModMailform.addEvents = function(settings) {
  * 
  * @param moduleId
  *            int - id текущего модуля
+ * @param baseUri
+ *            String - URL сайта
+ * @param moduleName
+ *            String - идентификатор модуля
  * 
  * @returns void
  */
-ModMailform.sendMessage = function(moduleId) {
+ModMailform.sendMessage = function(moduleId, baseUri, moduleName) {
 	Joomla.removeMessages();
 	ModMailform.hideForm(moduleId);
 	ModMailform.importCaptcha(moduleId);
@@ -120,8 +124,8 @@ ModMailform.sendMessage = function(moduleId) {
 	jQuery
 			.ajax({
 				type : "POST",
-				url : ModMailform.data[moduleId].baseUri + "index.php?option=com_ajax&module="
-						+ ModMailform.data[moduleId].moduleName + "&format=raw&Itemid="
+				url : baseUri + "index.php?option=com_ajax&module="
+						+ moduleName + "&format=raw&Itemid="
 						+ ModMailform.FRAME_CAPTCHA_MENU_ID,
 				data : form_data,
 				dataType : "text",
@@ -268,12 +272,9 @@ ModMailform.hideForm = function(moduleId) {
  * @returns void
  */
 ModMailform.showForm = function(moduleId) {
-	
-	if (jQuery("#" + this.FORM_CAPTCHA_HOLDER + "_" + moduleId).length != 0) {
-		this.loadCaptcha(moduleId);
-	}
+	this.loadCaptcha(moduleId);
 	// Задание размеров формы на экране
-	jQuery("#" + this.FORM_ID + "_" + moduleId).css({
+	jQuery("#" + this.FORM_WINDOW + "_" + moduleId).css({
         display : 'block'
     });
 }
